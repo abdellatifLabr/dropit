bc15863a67fa4c2f7499462fcbd9606ed25ab7da
Labreche Abdellatif <tifo.zelo@gmail.com>
2019-12-11 12:52:28 +0100

    media files root dir

--- dropit/settings.py
+++ dropit/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = 'files'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup',
+    'droplet'
 ]
 
 MIDDLEWARE = [
